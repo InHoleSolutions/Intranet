012480d7c61d27fe1eabfdebae04786cb4e2194a
InHoleSolutions <mgabriela.moreno@inholesolutions.com>
2026-05-14 16:14:57 -0600

    Avance de politicas.html

--- Intranet-Web/Js/supabase-config.js
+++ Intranet-Web/Js/supabase-config.js
@@ -1,6 +1,8 @@
-// supabase-config.js
-const SUPABASE_URL = 'https://yhdpktwvzwkhqvniwqmr.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloZHBrdHd2endraHF2bml3cW1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MDE1MDEsImV4cCI6MjA5NDE3NzUwMX0.QIjoCNFM7YkGxO6lGao7t10gKclohmqgd0JsAGhTFkY';
+// Js/supabase-config.js
 
-// Exportamos el cliente para usarlo en cualquier parte del proyecto
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Copia y pega tus llaves aquí directamente (las que tienes en el .env)
+const SUPABASE_URL = 'https://bgqlvkvlscujikhzzazf.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_mzHMY11jZ7OaMES008LTzA_d7Ya1A2g';
+
+// Usamos el objeto 'supabase' que cargamos desde el CDN en el HTML
+export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
