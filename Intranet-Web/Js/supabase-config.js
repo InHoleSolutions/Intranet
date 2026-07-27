e1708460125a5a43158b6469f127374ca84c210f
InHoleSolutions <mgabriela.moreno@inholesolutions.com>
2026-07-27 08:27:41 -0600

    Terminación de modificar la nueva public key y poner latidos a todos los sitios

--- Intranet-Web/Js/supabase-config.js
+++ Intranet-Web/Js/supabase-config.js
@@ -2,7 +2,7 @@
 
 // Copia y pega tus llaves aquí directamente (las que tienes en el .env)
 const SUPABASE_URL = 'https://bgqlvkvlscujikhzzazf.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_mzHMY11jZ7OaMES008LTzA_d7Ya1A2g';
+const SUPABASE_KEY = 'sb_publishable_S5G17esnd47Gw9hO3rTfbA_cTxVtG-P';
 
 // Usamos el objeto 'supabase' que cargamos desde el CDN en el HTML
 export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
